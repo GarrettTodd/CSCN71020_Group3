b6cfff353007397937e5143ff2b07cf66ef78aac
Tyler Scheifley <tylerscheifley@gmail.com>
2021-11-15 20:53:29 -0500

    Created unit tests for findingAngles() in triangleSolver

--- TriangleSolverTests/TriangleSolverTests.cpp
+++ TriangleSolverTests/TriangleSolverTests.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 #include <stdbool.h>
+
 extern "C" 
 {
 
@@ -20,6 +21,7 @@ namespace TriangleSolverTests
 	{
 	public:
 		
+		//analyzeTriangle
 		TEST_METHOD(analyzeTriangle_Invalid)
 		{
 			//testing analyzeTriangle with invalid sides: 1 2 3
@@ -130,6 +132,167 @@ namespace TriangleSolverTests
 			analyzeTriangle(triangleSides[0], triangleSides[1], triangleSides[2], &isTriangle);
 
 			Assert::AreEqual(Expected, isTriangle);
+
+		}
+
+		//findingAngles 
+
+		TEST_METHOD(findingAngles_Scalene_angle1)
+		{
+			// testing findingAngles to create a the first angle of a scalene
+			// expected outcome of function:40.535797641604667 
+
+			double triangleSides[3] = { 7, 5, 10 };
+			double Expected = 40.535797641604667;
+			
+			double angle;
+			findingAngles(triangleSides[0], triangleSides[1], triangleSides[2], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+			
+
+		}
+
+		TEST_METHOD(findingAngles_Scalene_angle2)
+		{
+			// testing findingAngles to create a the second angle of a scalene
+			// expected outcome of function:111.80373566125496 
+
+			double triangleSides[3] = { 7, 5, 10 };
+			double Expected = 111.80373566125496;
+				
+
+			double angle;
+			findingAngles(triangleSides[2], triangleSides[0], triangleSides[1], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
+
+		}
+
+
+		TEST_METHOD(findingAngles_Scalene_angle3)
+		{
+			// testing findingAngles to create a the Third angle of a scalene
+			// expected outcome of function:27.660446849299753
+
+			double triangleSides[3] = { 7, 5, 10 };
+			double Expected = 27.660446849299753;
+				
+
+
+			double angle;
+			findingAngles(triangleSides[1], triangleSides[2], triangleSides[0], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
+
+		}
+
+		TEST_METHOD(findingAngles_Equilatoral_angle1)
+		{
+			// testing findingAngles to create a the first angle of a Equilatoral
+			// expected outcome of function:59.999993384053134
+
+			double triangleSides[3] = { 7, 7, 7 };
+			double Expected = 59.999993384053134;
+
+			double angle;
+			findingAngles(triangleSides[0], triangleSides[1], triangleSides[2], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
+
+		}
+
+		TEST_METHOD(findingAngles_Equilatoral_angle2)
+		{
+			// testing findingAngles to create a the second angle of a Equilatoral
+			// expected outcome of function:59.999993384053134 
+
+			double triangleSides[3] = { 7, 7, 7 };
+			double Expected = 59.999993384053134;
+
+			double angle;
+			findingAngles(triangleSides[2], triangleSides[0], triangleSides[1], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
+
+		}
+
+		TEST_METHOD(findingAngles_Equilatoral_angle3)
+		{
+			// testing findingAngles to create a the third angle of a Equilatoral
+			// expected outcome of function:59.999993384053134 
+
+			double triangleSides[3] = { 7, 7, 7 };
+			double Expected = 59.999993384053134;
+
+			double angle;
+			findingAngles(triangleSides[1], triangleSides[2], triangleSides[0], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
+
+		} 
+
+
+		TEST_METHOD(findingAngles_Isosceles_angle1)
+		{
+			// testing findingAngles to create a the first angle of a Isosceles
+			// expected outcome of function:48.189679790548134
+
+			double triangleSides[3] = { 6, 6, 8 };
+			double Expected = 48.189679790548134;
+
+			double angle;
+			findingAngles(triangleSides[0], triangleSides[1], triangleSides[2], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
+
+		}
+
+
+		TEST_METHOD(findingAngles_Isosceles_angle2)
+		{
+			// testing findingAngles to create a the second angle of a Isosceles
+			// expected outcome of function:83.620620571063128
+
+			double triangleSides[3] = { 6, 6, 8 };
+			double Expected = 83.620620571063128;
+
+			double angle;
+			findingAngles(triangleSides[2], triangleSides[0], triangleSides[1], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
+
+		}
+
+		TEST_METHOD(findingAngles_Isosceles_angle3)
+		{
+			// testing findingAngles to create a the third angle of a Isosceles
+			// expected outcome of function:48.189679790548134
+
+			double triangleSides[3] = { 6, 6, 8 };
+			double Expected = 48.189679790548134;
+
+			double angle;
+			findingAngles(triangleSides[1], triangleSides[2], triangleSides[0], &angle);
+			Assert::AreEqual(Expected, angle);
+
+
+
 
 		}
 		
