f46a44b10dedec8d1fac19051b8ffe8839e2ee3e
Raghu Podipireddy <raghupodipireddy@gmail.com>
2021-11-21 15:49:10 -0500

    Formatted and added comments for rectangleSolver test cases. Added unit test case for negative only inputs of checkOrthogonality()

--- PolygonCheckerTests/PolygonCheckerTests.cpp
+++ PolygonCheckerTests/PolygonCheckerTests.cpp
@@ -461,8 +461,12 @@ namespace PolygonCheckerTests
 	{
 	public:
 
-		TEST_METHOD(distanceBetween_TwoDiffPoints)
+		//Testing distanceBetween()
+		TEST_METHOD(distanceBetween_PositiveOnlyPoints_PositiveLength)
 		{
+			//testing the distanceBetween() function of 2 random positive points
+			//it will return the shortest possible length between these two points which should be 8.485 
+			//a 3rd parameter for AreEqual() is used to determine precision to avoid a failing test case due to precision
 			Point a; 
 			a.x = 6;
 			a.y = 10;
@@ -475,8 +479,11 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result, 3);
 		}
-		TEST_METHOD(distanceBetween_SamePoints)
+
+		TEST_METHOD(distanceBetween_SamePoints_ZeroLength)
 		{
+			//testing the distanceBetween() function of 2 points that are the same
+			//it should return 0 as there is no distance between them
 			Point a;
 			a.x = 7;
 			a.y = -4;
@@ -489,8 +496,11 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result);
 		}
-		TEST_METHOD(distanceBetween_NegativeOnlyPoints)
+
+		TEST_METHOD(distanceBetween_NegativeOnlyPoints_PositiveLength)
 		{
+			//testing the distanceBetween() function of 2 random negative points
+			//it will return the shortest possible length between these two points which should be 19.416488 
 			Point a;
 			a.x = -7;
 			a.y = -15;
@@ -503,8 +513,11 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result, 3);
 		}
-		TEST_METHOD(distanceBetween_NegativeAndPositivePoints)
+
+		TEST_METHOD(distanceBetween_NegativeAndPositivePoints_PositiveLength)
 		{
+			//testing the distanceBetween() function of 2 points that have both positive and negative values
+			//it will return the shortest possible length between these two points which should be 53.225934 
 			Point a;
 			a.x = 14;
 			a.y = -31;
@@ -517,8 +530,13 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result, 3);
 		}
-		TEST_METHOD(getPerimeter_NonRectangle)
+
+		//Testing getPerimeter()
+		TEST_METHOD(getPerimeter_NonRectangle_PositiveValuePerimeter)
 		{
+			//testing the getPerimeter() function of points that do not form a rectangle
+			//It should return 43.766 as the shapes perimeter
+			//A 3rd parameter is used for AreEqual() for precision to avoid a failing test case due to precision
 			Point points[4];
 			points[0].x = 6;
 			points[0].y = 10;
@@ -534,8 +552,11 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result, 3);
 		}
-		TEST_METHOD(getPerimeter_DiagonalLineRectangle)
+
+		TEST_METHOD(getPerimeter_DiagonalLineRectangle_PositivePerimeterValue)
 		{
+			//testing the getPerimeter() function of points that do form a rectangle with only diagonal lines
+			//It should return 39.598 as the shapes perimeter
 			Point points[4];
 			points[0].x = 6;
 			points[0].y = 10;
@@ -551,8 +572,11 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result, 3);
 		}
-		TEST_METHOD(getPerimeter_HorizontalandVerticalLineRectangle_66)
+
+		TEST_METHOD(getPerimeter_HorizontalandVerticalLineRectangle_PostivePerimeterValue)
 		{
+			//testing the getPerimeter() function of points that do form a rectangle with only vertical and horizontal lines
+			//It should return 66.000 as the shapes perimeter
 			Point points[4];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -568,8 +592,11 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result);
 		}
-		TEST_METHOD(getPerimeter_PointsEnteredAreSame_0)
+
+		TEST_METHOD(getPerimeter_PointsEnteredAreSame_ZeroPerimeter)
 		{
+			//testing the getPerimeter() function of points that are all the same
+			//It should return 0.0 as the shapes perimeter as the points do not create a shape
 			Point points[4];
 			points[0].x = 4;
 			points[0].y = 9;
@@ -585,8 +612,13 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result);
 		}
-		TEST_METHOD(getArea_DiagonalLineRectangle_96)
+
+		//Testing getArea()
+		//only 2 cases is performed as area is calculated after shape is confirmed to be a rectangle
+		TEST_METHOD(getArea_DiagonalLineRectangle_PositiveAreaValue)
 		{
+			//testing the getArea() function of points that create a diagonal line rectangle
+			//it should return 96.000 as the shapes area
 			Point points[3];
 			points[0].x = 6;
 			points[0].y = 10;
@@ -600,9 +632,11 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result);
 		}
-		TEST_METHOD(getArea_HorizontalandVerticalLineRectangle_230)
+
+		TEST_METHOD(getArea_HorizontalandVerticalLineRectangle_PositiveAreaValue)
 		{	
-			//assume points have been sorted as the sorting takes place before this function is called in program
+			//testing the getArea() function of points that create a vertical and horizontal line rectangle
+			//it should return 230.000 as the shapes area
 			Point points[3];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -616,8 +650,12 @@ namespace PolygonCheckerTests
 
 			Assert::AreEqual(expected, result);
 		}
-		TEST_METHOD(checkOrthogonality_DiagonalLineRectangle)
+
+		//Testing checkOrthogonality()
+		TEST_METHOD(checkOrthogonality_DiagonalLineRectangle_True)
 		{
+			//Testing the checkOrthogonality() function of 3 points that can create a diagonal line rectangle
+			//Function returns true only if the 1st and 3rd points intersect with the 2nd point in a way that would create a 90 degree angle
 			Point points[3];
 			points[0].x = 6;
 			points[0].y = 10;
@@ -628,8 +666,11 @@ namespace PolygonCheckerTests
 
 			Assert::IsTrue(checkOrthogonality(points[0], points[1], points[2]));
 		}
-		TEST_METHOD(checkOrthogonality_VerticalAndHorizontalLineRectangle)
+
+		TEST_METHOD(checkOrthogonality_VerticalAndHorizontalLineRectangle_True)
 		{
+			//Testing the checkOrthogonality() function of 3 points that can create a vertical and horizontal line rectangle
+			//Function returns true only if the 1st and 3rd points intersect with the 2nd point in a way that would create a 90 degree angle
 			Point points[3];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -640,8 +681,11 @@ namespace PolygonCheckerTests
 
 			Assert::IsTrue(checkOrthogonality(points[0], points[1], points[2]));
 		}
-		TEST_METHOD(checkOrthogonality_AcuteAngle)
+
+		TEST_METHOD(checkOrthogonality_AcuteAngle_False)
 		{
+			//Testing the checkOrthogonality() function of 3 points when connected, would create an acute angle
+			//Function returns false if the 1st and 3rd points intersect the 2nd point in a way that would not create a 90 degree angle
 			Point points[3];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -652,8 +696,11 @@ namespace PolygonCheckerTests
 
 			Assert::IsFalse(checkOrthogonality(points[0], points[1], points[2]));
 		}
-		TEST_METHOD(checkOrthogonality_ObtuseAngle)
+
+		TEST_METHOD(checkOrthogonality_ObtuseAngle_False)
 		{
+			//Testing the checkOrthogonality() function of 3 points when connected, would create an obtuse angle
+			//Function returns false if the 1st and 3rd points intersect the 2nd point in a way that would not create a 90 degree angle
 			Point points[3];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -664,8 +711,11 @@ namespace PolygonCheckerTests
 
 			Assert::IsFalse(checkOrthogonality(points[0], points[1], points[2]));
 		}
-		TEST_METHOD(checkOrthogonality_StraightAngle)
+
+		TEST_METHOD(checkOrthogonality_StraightAngle_False)
 		{
+			//Testing the checkOrthogonality() function of 3 points when connected, would create a straight angle (straight line)
+			//Function returns false if the 1st and 3rd points intersect the 2nd point in a way that would not create a 90 degree angle
 			Point points[3];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -676,8 +726,28 @@ namespace PolygonCheckerTests
 
 			Assert::IsFalse(checkOrthogonality(points[0], points[1], points[2]));
 		}
-		TEST_METHOD(analyzePoints_DiagonalLineRectangle)
+
+		TEST_METHOD(checkOrthogonality_NegativeValuePointOnly_True)
 		{
+			//Testing the checkOrthogonality() function of 3 points of only negative coordinates that can create a vertical and horizontal line rectangle
+			//Function returns true only if the 1st and 3rd points intersect with the 2nd point in a way that would create a 90 degree angle
+			Point points[3];
+			points[0].x = -13;
+			points[0].y = -5;
+			points[1].x = -7;
+			points[1].y = -5;
+			points[2].x = -7;
+			points[2].y = -18;
+
+			Assert::IsTrue(checkOrthogonality(points[0], points[1], points[2]));
+		}
+
+		//Testing analyzePoints()
+		TEST_METHOD(analyzePoints_DiagonalLineRectangle_True)
+		{
+			//testing the analyzePoints() function of points that create a diagonal line rectangle
+			//checks different combinations of these points to see if any form 90 degree angles with eachother
+			//returns true if when all points are connected, they each form a 90 degree angle indicating a rectangle
 			Point points[4];
 			points[0].x = 6;
 			points[0].y = 10;
@@ -690,8 +760,11 @@ namespace PolygonCheckerTests
 
 			Assert::IsTrue(analyzePoints(points));
 		}
-		TEST_METHOD(analyzePoints_VerticalAndHorizontalLineRectangle)
+
+		TEST_METHOD(analyzePoints_VerticalAndHorizontalLineRectangle_True)
 		{
+			//testing the analyzePoints() function of points that create a vertical and horizontal line rectangle
+			//returns true if when all points are connected, they each form a 90 degree angle indicating a rectangle
 			Point points[4];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -704,8 +777,11 @@ namespace PolygonCheckerTests
 
 			Assert::IsTrue(analyzePoints(points));
 		}
-		TEST_METHOD(analyzePoints_NonRectangle)
+
+		TEST_METHOD(analyzePoints_NonRectangle_False)
 		{
+			//testing the analyzePoints() function of points that don't create a rectangle
+			//returns false if when all points are connecting, not all of them form 90 degree angles indicating a non-rectangle
 			Point points[4];
 			points[0].x = 2;
 			points[0].y = 9;
@@ -718,8 +794,12 @@ namespace PolygonCheckerTests
 
 			Assert::IsFalse(analyzePoints(points));
 		}
-		TEST_METHOD(analyzePoints_DiagonalLineRectangle_PointsNotInOrder)
+
+		TEST_METHOD(analyzePoints_DiagonalLineRectanglePointsNotInOrder_True)
 		{
+			//testing the analyzePoints() function of points that create a diagonal line rectangle but inputted out of order
+			//creates multiple combinations of these points to see if the shape made creates 90 degree angles
+			//returns true if one or more combinations show all points having 90 degree angles
 			Point points[4];
 			points[0].x = 6;
 			points[0].y = 10;
@@ -732,8 +812,12 @@ namespace PolygonCheckerTests
 
 			Assert::IsTrue(analyzePoints(points));
 		}
-		TEST_METHOD(analyzePoints_VerticalAndHorizontalLineRectangle_PointsNotInOrder)
+
+		TEST_METHOD(analyzePoints_VerticalAndHorizontalLineRectanglePointsNotInOrder_True)
 		{
+			//testing the analyzePoints() function of points that create a vertical and horizontal line rectangle but inputted out of order
+			//creates multiple combinations of these points to see if the shape made creates 90 degree angles
+			//returns true if one or more combinations show all points having 90 degree angles
 			Point points[4];
 			points[0].x = 3;
 			points[0].y = 15;
@@ -746,8 +830,11 @@ namespace PolygonCheckerTests
 
 			Assert::IsTrue(analyzePoints(points));
 		}
-		TEST_METHOD(analyzePoints_AllSamePoints)
+
+		TEST_METHOD(analyzePoints_AllSamePoints_False)
 		{
+			//testing the analyzePoints() function of points that are all the same
+			//checks if the points entered are the same and returns false if they are
 			Point points[4];
 			points[0].x = 17;
 			points[0].y = -15;
@@ -760,6 +847,7 @@ namespace PolygonCheckerTests
 
 			Assert::IsFalse(analyzePoints(points));
 		}
+
 	};
 
 	TEST_CLASS(MiscellaneousTests)
